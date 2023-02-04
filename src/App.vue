273a816f2aaf290a7157d979ffc3d72ddefa7eba
Арсен Керимов <113138301+KerArSis@users.noreply.github.com>
2023-02-04 18:32:50 +0300

    task_17

--- src/App.vue
+++ src/App.vue
@@ -2,26 +2,16 @@
   export default {
     data() {
       return {
-        num1: 1,
-        num2: 2,
-        text: 'Sum'
+        visible1: true,
+        visible2: false,
       }
     },
-    methods: {
-      show: function() {
-      let text = this.cape(this.text)
-      alert(this.num1 + this.num2);
-      },
-      cape: function(str) {
-      return str[0].toUpperCase() + str.slice(1);
-      }
-    }
   }
 </script>
 
 <template>
-  <a @click.prevent href="https://github.com/KerArSis">GitHub</a>
-  <button @click.once="show">click</button>
+  <div v-if="visible1">True</div>
+  <div v-if="!visible2">False</div>
 </template>
 
 <style>
