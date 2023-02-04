c9ecb62be883feca12711a5a323b2eb2a3062a3d
Арсен Керимов <113138301+KerArSis@users.noreply.github.com>
2023-02-04 17:46:34 +0300

    task_13

--- src/App.vue
+++ src/App.vue
@@ -2,27 +2,22 @@
   export default{
     data(){
       return{
-        date: '1'
       }
     },
     methods: {
-      show: function(){
-        let date = this.dayOfWeek(this.date);
-        const dayOfWeekDigit = new Date().getDay();
-        let days = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье']
-        alert(date);
+      show: function(str){
+        alert(str*str);
       },
-      dayOfWeek: function(date){
-        let days = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье']
-        return days[date];
-      },
+      show1: function(str1){
+        alert(str1*str1)
+      }
     }
   }
 </script>
 
 <template>
-  <button class="button" @click="show">Day</button>
-  {{ dayOfWeekDigit }}
+  <button @click="show(2)">квадрат числа 2</button>
+  <button @click="show1(3)">квадрат числа 3</button>
 </template>
 
 <style>
