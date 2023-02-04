d06633c829f8e87e2655761d194b8bc56dd45e38
Арсен Керимов <113138301+KerArSis@users.noreply.github.com>
2023-02-04 15:42:34 +0300

    task_5

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,17 @@
 <script>
-  export default {
-    data() {
-      return {
-        name:'Arsen',
-        surn:'Kerimov',
-      }
+export default {
+  data() {
+    return {
+      text: 'page',
+      href: 'page.html',
     }
   }
+}
 </script>
-
 <template>
-  <div> {{name}} </div>
-  <div> {{surn}} </div>
+  <a href="page.html">page</a>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<style>
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
