bd63f9d1e13bfba3eaa277e6aad39e4cd68982ea
Арсен Керимов <113138301+KerArSis@users.noreply.github.com>
2023-02-04 16:15:16 +0300

    task_8

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,14 @@
   export default{
     data(){
       return{
-        arr: [1, 2, 3],
-        arr2: ['x', 'y', 'z'],
+        obj: {x: 1, y: 2, z: 3},
       }
     }
   }
 </script>
 
 <template>
-  <div>
-    <p> {{arr2[0]}} </p>
-    <p> {{arr2[1]}} </p>
-    <p> {{arr2[2]}} </p>
-    {{arr[0]+ arr[1]+arr[2]}}
-  </div>
+    {{ obj.x + obj.y + obj.z }}
 </template>
 
 <style>
