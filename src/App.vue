142c54bccd40c77491c9264ba940773f1afd70c4
Арсен Керимов <113138301+KerArSis@users.noreply.github.com>
2023-02-04 16:24:54 +0300

    task_9

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <script>
   export default{
     data(){
-      return{
-        obj: {x: 1, y: 2, z: 3},
+      
+    },
+    methods: {
+      show: function(){
+        alert(new Date());
       }
     }
   }
 </script>
 
 <template>
-    {{ obj.x + obj.y + obj.z }}
+    {{ show() }}
 </template>
 
 <style>
