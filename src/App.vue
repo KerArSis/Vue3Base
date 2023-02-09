a012e991f9f5d895787bb376ed2ab5eb88a3154c
Арсен Керимов <113138301+KerArSis@users.noreply.github.com>
2023-02-09 19:10:11 +0300

    task_48

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,45 @@
 <script>
 export default{
 data(){
-return{
-    languages: [],
-}
+  return{
+    language: '',
+    visible: false,
+    visible1: false,
+    visible2: false,
+  }
+},
+methods: {
+one(){
+      this.visible = true;
+},
+  two() {
+    this.visible1 = true;
+  },
+  three() {
+    this.visible2 = true;
+  }
 }
 }
 </script>
 
 <template>
-<div>
-<input type="checkbox" v-model="languages" value="Информатика">
-<label>Информатика</label>
+<div class="hunter">
+<p>Какой язык ваш родной?</p>
+<input name="radio" type="radio" v-model="language" value="Крымскотатарский" @click="one">
+<label for="">Крымскотатарский</label>
 <br>
-<input type="checkbox" v-model="languages" value="Математика">
-<label>Математика</label>
+<input name="radio" type="radio" v-model="language" value="Русский" @click="two">
+<label for="">Русский</label>
 <br>
-<input type="checkbox" v-model="languages" value="Английский">
-<label>Английский</label>
-<ol>
-<li v-for="elem in languages">{{ elem }}</li>
-</ol>
+<input name="radio" type="radio" v-model="language" value="Испанский" @click="three">
+<label for="">Испанский</label>
 </div>
+<p v-if="visible">Хош кельдинъиз</p>
+<p v-else-if="visible1">Добро пожаловать</p>
+<p v-else-if="visible2">Bienvenido</p>
+<p v-else=''> </p>
+
+<p>Ваш выбор: {{ language }}</p>
 <br>
 </template>
 
