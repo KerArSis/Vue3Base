37755df84d802b03c2a92e1fdb4c8a43f6288811
Арсен Керимов <113138301+KerArSis@users.noreply.github.com>
2023-02-09 19:12:50 +0300

    task_49

--- src/App.vue
+++ src/App.vue
@@ -2,45 +2,22 @@
 export default{
 data(){
   return{
-    language: '',
-    visible: false,
-    visible1: false,
-    visible2: false,
+    sity: 'Симферополь',
   }
 },
 methods: {
-one(){
-      this.visible = true;
-},
-  two() {
-    this.visible1 = true;
-  },
-  three() {
-    this.visible2 = true;
-  }
 }
 }
 </script>
 
 <template>
-<div class="hunter">
-<p>Какой язык ваш родной?</p>
-<input name="radio" type="radio" v-model="language" value="Крымскотатарский" @click="one">
-<label for="">Крымскотатарский</label>
-<br>
-<input name="radio" type="radio" v-model="language" value="Русский" @click="two">
-<label for="">Русский</label>
-<br>
-<input name="radio" type="radio" v-model="language" value="Испанский" @click="three">
-<label for="">Испанский</label>
-</div>
-<p v-if="visible">Хош кельдинъиз</p>
-<p v-else-if="visible1">Добро пожаловать</p>
-<p v-else-if="visible2">Bienvenido</p>
-<p v-else=''> </p>
-
-<p>Ваш выбор: {{ language }}</p>
-<br>
+  <p>В каком городе вы живете?</p>
+    <select v-model="sity">
+      <option>Симферополь</option>
+      <option>Бахчисарай</option>
+      <option>Керчь</option>
+    </select>
+    <p>{{ sity }}</p>
 </template>
 
 <style>
