a75b3d831bf5bccb6a921532d3e3518918c8f4a0
Арсен Керимов <113138301+KerArSis@users.noreply.github.com>
2023-02-09 18:34:21 +0300

    task_36

--- src/App.vue
+++ src/App.vue
@@ -2,49 +2,18 @@
 export default{
 data(){
 return{
-    Names: ['Alex','Jason','Paul']
 }
-},
-methods: {
-add: function(){
-this.Names.push('Mila');
-},
-del: function(){
-this.Names.shift();
-},
-del1: function () {
-this.Names.pop();
-},
-splice: function(){
-this.Names.splice(2, 1);
-},
-sort: function(){
-this.Names.sort();
-},
-reverse: function () {
-    this.Names.reverse();
-},
 }
 }
 </script>
 
 <template>
-<ul>
-<li v-for="elem in Names">{{ elem }}</li>
-</ul>
-<button @click="add">1</button>
-<br>
-<button @click="del">2</button>
-<br>
-<button @click="del1">3</button>
-<br>
-<button @click="splice">4</button>
-<br>
-<button @click="sort">5</button>
-<br>
-<button @click="reverse">6</button>
+<p>Alex</p>
+<p1>Jason</p1>
 </template>
 
 <style>
-
+  p {
+    color: red;
+  }
 </style>
