638e6336663bdbb985bbffbfb5d32851b7aff136
Арсен Керимов <113138301+KerArSis@users.noreply.github.com>
2023-02-09 17:35:20 +0300

    task_22

--- src/App.vue
+++ src/App.vue
@@ -2,32 +2,14 @@
   export default{
     data(){
       return{
-        visible: true,
-        visible2: true,
-        visible3: true,
-      }
-    },
-    methods: {
-      Alex: function() {
-        this.visible = !this.visible;
-      },
-      Jason: function () {
-        this.visible2 = !this.visible2;
-      },
-      Paul: function () {
-        this.visible3 = !this.visible3;
+        isAuth: false,
       }
     }
   }
 </script>
-
 <template>
-  <button @click="Alex">Alex</button>
-  <p>{{visible ? 'Alex' : '!'}}</p>
-  <button @click="Jason">Jason</button>
-  <p>{{visible2 ? 'Jason' : '!'}}</p>
-  <button @click="Paul">Paul</button>
-  <p>{{visible3 ? 'Paul' : '!'}}</p>
+  <p v-if="isAuth">+++</p>
+  <p v-else>---</p>
 </template>
 
 <style>
