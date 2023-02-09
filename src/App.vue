aaa2ca4e57caf08e14f3398b8a9829e46413c82c
Арсен Керимов <113138301+KerArSis@users.noreply.github.com>
2023-02-09 18:58:46 +0300

    task_44

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,35 @@
-<script>
+script>
 export default{
 data(){
 return{
-message: 'Arsen',
-number: 'Kerimov',
+    num: 0,
+    res: 0,
+sum: 0,
+num1: 0, 
+num2: 0,
 }
 },
 methods: {
+Calc: function () {
+    this.res = Math.sqrt(this.num);
+},
+Sum:function(){
+    this.sum = this.num1 + this.num2;
+}
 }
 }
 </script>
 
 <template>
-<input class="text-field__input" v-model="message">
-<p :style="{textTransform: 'uppercase'}">{{ message }}</p>
+<p>{{ res }}</p>
+<input class="text-field__input" v-model="num">
+<button @click="Calc">sqrt</button>
 <br>
-<input class="text-field__input" v-model="number">
-<p>{{ number*number }}</p>
+<p>{{ sum }}</p>
+<input class="text-field__input" v-model=num1>
+<input class="text-field__input" v-model=num2>
+<button @click="Sum">сумма</button>
+<br>
 </template>
 
 <style>
