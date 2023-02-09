f74aea89971269a8077a935d7f4f538c639d63a3
Арсен Керимов <113138301+KerArSis@users.noreply.github.com>
2023-02-09 18:41:01 +0300

    task_38

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,19 @@
 <script>
-export default{
-data(){
-return{
-    cssClasses: 'text',
-}
-}
-}
+  export default{
+    data(){
+      return{
+        cssClasses: 'BMW',
+        obj: {
+          done: true,
+          selected: false,
+        },
+      }
+    },
+  }
 </script>
 <template>
-<p :class="cssClasses">text</p>
+  <p :class="selected">BMW</p>
+  <p :class="obj">Alex</p>
 </template>
 
 <style>
