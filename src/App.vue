506193fed4f899910b52ae04bb07fec3f06a3879
Арсен Керимов <113138301+KerArSis@users.noreply.github.com>
2023-02-09 18:00:51 +0300

    task_29

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,30 @@
   export default{
     data(){
       return{
-        arr: ['x', 'y', 'z'],
+        obj: {a:'100$', b:'200$', c:'300$'},
       }
     }
   }
 </script>
 <template>
-  <ul v-for="(elem, key) in arr"> {{ key }} {{ elem }}</ul>
+<ul>
+    <li v-for="elem in obj">{{ elem }}</li>
+</ul>
+<br>
+<br>
+<ul>
+    <li v-for="(key, elem) in obj"> {{ elem }} - {{ key }}</li>
+</ul>
+<br>
+<br>
+<ul>
+    <li v-for="(key, elem, index) in obj"> {{ elem }} - {{ key }} - {{ index }}</li>
+</ul>
+<br>
+<br>
+<ul>
+    <li v-for="(key, elem, index) in isSmeshariki"> {{ elem }} - {{ key }} - {{ index+1 }}</li>
+</ul>
 </template>
 
 <style>
