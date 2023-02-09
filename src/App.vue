6943bb8c8597132ed1ea51c3bbd6d65f6460403e
Арсен Керимов <113138301+KerArSis@users.noreply.github.com>
2023-02-09 18:47:07 +0300

    task_40

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,13 @@
 <script>
-export default{
-data(){
-return{
-    cssClasses: 'BMW',
-    visible: true,
-    style: {
-    hidden: true,
-}
-}
-},
-methods: {
-setDone:function(){
-this.style.hidden = false;
-},
-setTwo: function(){
-this.style.hidden = true;
-},
-bmw: function () {
-    this.visible = !this.visible;
-}
-}
-}
+  export default{
+    data(){
+      return{
+      }
+    },
+  }
 </script>
-
 <template>
-<button @click="bmw">BMW</button>
-<p class="BMW" v-if="visible">BMW</p>
-<p :class="style">Conclave</p>
-<button @click="setDone">показывает</button>
-<button @click="setTwo">прячет</button>
+  <p :class="{active: true, error: false}">Conclave</p>
 </template>
 
 <style>
