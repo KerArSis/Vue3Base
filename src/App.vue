5cdff9928ab8a460eb381ba8fcb4bb41b3f59fe9
Арсен Керимов <113138301+KerArSis@users.noreply.github.com>
2023-02-09 18:11:31 +0300

    task_31

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,48 @@
 <script>
-  export default{
-    data(){
-      return{
-        obj: {a:'100$', b:'200$', c:'300$'},
-      }
-    }
-  }
+export default{
+data(){
+return{
+    hrefs: [
+    {href: '1.html',text: 'text1',},
+    {href: '2.html',text: 'text2',},
+    {href: '3.html',text: 'text3',},
+    ],
+    products: [
+    {
+        name: 'product1',
+        price: 100,
+        quantity: 5
+    },
+    {
+        name: 'product2',
+        price: 200,
+        quantity: 4
+    },
+    {
+        name: 'product3',
+        price: 300,
+        quantity: 3
+    },
+    ]
+}
+}
+}
 </script>
-<template>
-<ul>
-    <li v-for="elem in obj">{{ elem }}</li>
-</ul>
-<br>
-<br>
-<ul>
-    <li v-for="(key, elem) in obj"> {{ elem }} - {{ key }}</li>
-</ul>
-<br>
-<br>
-<ul>
-    <li v-for="(key, elem, index) in obj"> {{ elem }} - {{ key }} - {{ index }}</li>
-</ul>
-<br>
-<br>
-<ul>
-    <li v-for="(key, elem, index) in isSmeshariki"> {{ elem }} - {{ key }} - {{ index+1 }}</li>
-</ul>
-</template>
 
+<template>
+<ul class="styled">
+    <li v-for="link in hrefs"><a href="{{ link.href }}">{{ link.text }}</a></li>
+</ul>
+<br>
+<br>
+<table>
+    <tr v-for="col in products">
+    <td>{{ col.name }}</td>
+    <td>{{ col.price }}</td>
+    <td>{{ col.quantity }}</td>
+    </tr>
+</table>
+</template>
 <style>
 
 </style>
