2b86355bcffca0ec3af0be33c9310a704a3fbb7b
Арсен Керимов <113138301+KerArSis@users.noreply.github.com>
2023-02-10 13:51:56 +0300

    task_52

--- src/App.vue
+++ src/App.vue
@@ -2,32 +2,19 @@
 export default{
 data(){
 return{
-    isDisabled: true,
-    isDisabled1: true,
-    checked: false,
+Alex: ' ',
+Jason: ' '
 }
-},
-methods: {
-BMW: function () {
-    this.isDisabled = false;
-},
-bmw: function () {
-    this.isDisabled1 = false;
-},
 }
 }
 </script>
+
 <template>
-<input type="text" class="text-field__input"  v-bind:disabled="isDisabled">
+<input class="text-field__input" v-model="Alex">
+<p>{{ Alex }}</p>
 <br>
-<button @click="BMW">BMW</button>
-<br>
-<br>
-<br>
-<input type="text" class="text-field__input" v-bind:disabled="isDisabled1">
-<br>
-<input type="checkbox" v-model="checked" @click="bmw">
-<button @click="BMW">BMW</button> 
+<a href="#" class="inline-link-1" >Alex</a>
+<p ></p>
 </template>
 
 <style>
