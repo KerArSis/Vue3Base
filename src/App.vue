a2ad829ad553b7e55bda39d64c7cf66f6e45b99b
Арсен Керимов <113138301+KerArSis@users.noreply.github.com>
2023-02-10 14:02:43 +0300

    task_55

--- src/App.vue
+++ src/App.vue
@@ -2,27 +2,46 @@
 export default{
 data(){
 return{
-    bmw: '',
-    BMW: ['Alex', 'Jason', 'Paul'],
+    students: [
+    {
+        id: 1,
+        name: 'Arsen',
+        surn: 'Kerimov',
+        salary: 100,
+				age: 19,
+    },
+    {
+        id: 2,
+        name: 'ALex',
+        surn: 'Bane',
+        salary: 200,
+				age: 18,
+    },
+    {
+        id: 3,
+        name: 'Jason',
+        surn: 'Lightwood',
+        salary: 300,
+				age: 25,
+    },
+    ],
 }
 },
 methods: {
-removeBMW: function (index) {
-    this.BMW.splice(index, 1);
+removeStudents: function (id) {
+    this.students = this.students.filter((student) => {
+    return student.id !== id;
+    })
 }
 }
 }
 </script>
 
 <template>
-<ul>
-<li v-for="(item, index) in 
-			BMW" :key="index">
-    {{ item }}
-    <br>
-    <button @click="removeBMW(index)">remove</button>
-</li>
-</ul>
+<table>
+<tr v-for="student in students" :key="student.id"> {{ student.name }} {{ student.surn }} {{ student.salary }} {{ student.age }} <button class="button" @click="removeStudents(students.id)">remove</button>
+</tr>
+</table>
 </template>
 
 <style>
